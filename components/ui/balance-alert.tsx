20d0b233cbade90de7a6ee8c464e758352bd46ef
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-04-19 21:48:58 -0700

    Added Random Stuff

--- components/ui/balance-alert.tsx
+++ components/ui/balance-alert.tsx
@@ -2,12 +2,16 @@ import { Terminal } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "./alert";
 import { useAccount } from "wagmi";
 import { useBalance } from "wagmi";
+import { useGasPrice } from "wagmi";
+import { useBlock } from "wagmi";
 
 export default function BalanceAlert() {
   const { address } = useAccount();
   const { data } = useBalance({
     address,
   });
+  const { data: gasData } = useGasPrice({});
+  const { data: block } = useBlock({});
 
   return (
     <div className="">
@@ -17,6 +21,12 @@ export default function BalanceAlert() {
           <AlertTitle>Heads up!</AlertTitle>
           <AlertDescription>
             Your current balance is: {data?.formatted} {data?.symbol}
+          </AlertDescription>
+          <AlertDescription>
+            Estimated Gas Cost is: {gasData?.toString()} {data?.symbol}
+          </AlertDescription>
+          <AlertDescription>
+            Current Block Number: {block?.number.toString()}
           </AlertDescription>
         </Alert>
       </div>
